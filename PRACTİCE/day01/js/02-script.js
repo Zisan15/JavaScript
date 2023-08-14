08e4dc7d36143f15c0b8ba8feccfe962089eecc3
Zisan <zikaplan42@gmail.com>
2023-08-14 18:20:05 +0300

    practice

---
+++ PRACTİCE/day01/js/02-script.js
@@ -0,0 +1,26 @@
+//!ornek-1
+// diktortgenin alanini hesaplayan kodu yaziniz
+let uzunKenar=prompt("lutfen uzun kenari giriniz")
+let kisaKenar=prompt("lutfen kisa kenari giriniz")
+
+let alan=Number(kisaKenar*uzunKenar)
+
+window.alert(`dikdortgenin alani ${alan}`)
+
+//!ornek-2
+let piNumber=3.14;
+console.log(typeof(piNumber))
+let str="Satranc";
+console.log(typeof(str))
+
+let x=null;
+let y=undefined;
+let z="";
+console.log(Boolean(x))
+console.log(Boolean(y))
+console.log(Boolean(z))
+
+
+//!ornek-3
+
+
