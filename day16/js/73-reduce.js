b5de931f3aab082f20355ce903d7f35e37eb18da
Zisan <zikaplan42@gmail.com>
2023-08-27 12:45:33 +0300

    düzenleme

--- day16/js/73-reduce.js
+++ day16/js/73-reduce.js
@@ -1,11 +1,13 @@
 const arr = [23, 5, 74, 7];
 
+
 const sum = arr.reduce((acc, cur) => {
     console.log(acc, cur);
     return acc + cur;
 }, 0);
 
 console.log(sum);
+
 
 const multiply = arr.reduce((acc, cur) => {
     console.log(acc, cur);
--- day16/js/74-reduce2.js
+++ day16/js/74-reduce2.js
@@ -1,5 +1,6 @@
 const salaries = [4000, 3500, 2000, 2500, 5000];
 
+
 const totalSalary = salaries
     .filter((salary) => salary < 3000)
     .map((salary) => salary * 1.1)
@@ -7,6 +8,8 @@ const totalSalary = salaries
 
 
 console.log(totalSalary);
+
+
 
 let toplam = 0;
 for (const salary of salaries) {
